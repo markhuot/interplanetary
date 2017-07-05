76d6314bcd8f7d7b7ea56c4c772602178209109f
Mark Huot <mark@markhuot.com>
2017-07-05 17:20:01 -0400

    more container usage

--- components/ListBox/ListBox.jsx
+++ components/ListBox/ListBox.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import Container from 'Component/Container';
 import s from './styles.css';
 
 class ListBox extends React.Component {
@@ -9,14 +10,16 @@ class ListBox extends React.Component {
       attr.push(s.bleeds);
     }
 
-    return <div className={attr.join(' ')}>
-      <h3 className={s.title}>FAQs</h3>
-      <ul className={s.list}>
-        <li><a href="#">Is interplanetary travel safe for pregnant women?</a></li>
-        <li><a href="#">Can I arrange a payment plan?</a></li>
-        <li><a href="#">Will I encounter other life forms on my travels?</a></li>
-      </ul>
-    </div>;
+    return <Container>
+      <div className={attr.join(' ')}>
+        <h3 className={s.title}>FAQs</h3>
+        <ul className={s.list}>
+          <li><a href="#">Is interplanetary travel safe for pregnant women?</a></li>
+          <li><a href="#">Can I arrange a payment plan?</a></li>
+          <li><a href="#">Will I encounter other life forms on my travels?</a></li>
+        </ul>
+      </div>
+    </Container>;
   }
 }
 
--- components/LocationCarousel/LocationCarousel.jsx
+++ components/LocationCarousel/LocationCarousel.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+import Container from 'Component/Container';
+
 import s from './styles.css';
 
 import hoagImage from './hoag.png';
@@ -16,27 +19,29 @@ class LocationCarousel extends React.Component {
     }
 
     return <div className={attr.join(' ')}>
-      <div className={s.heading}>
-        <h3 className={s.title}>Featured Locations</h3>
-        <p><a className={s.cta} href="#">See all locations</a></p>
-      </div>
-      <ul className={s.cards}>
-        <li className={s.card} style={{backgroundImage: `url(${hoagImage})`}}>
-          <p className={s.content}><small className={s.small}>Hoag's Object</small> BOXX-3W Condos</p>
-        </li>
-        <li className={s.card} style={{backgroundImage: `url(${demiosImage})`}}>
-          <p className={s.content}><small className={s.small}>Demios, Mars</small> Kasai</p>
-        </li>
-        <li className={s.card} style={{backgroundImage: `url(${cassiniImage})`}}>
-          <p className={s.content}><small className={s.small}>Cassini, Jupiter</small> The Dustbowl</p>
-        </li>
-        <li className={s.card} style={{backgroundImage: `url(${ringImage})`}}>
-          <p className={s.content}><small className={s.small}>Outer Ring</small> Ptavvs</p>
-        </li>
-        <li className={s.card} style={{backgroundImage: `url(${enceladusImage})`}}>
-          <p className={s.content}><small className={s.small}>Enceladus</small> Ganymede</p>
-        </li>
-      </ul>
+      <Container>
+        <div className={s.heading}>
+          <h3 className={s.title}>Featured Locations</h3>
+          <p><a className={s.cta} href="#">See all locations</a></p>
+        </div>
+        <ul className={s.cards}>
+          <li className={s.card} style={{backgroundImage: `url(${hoagImage})`}}>
+            <p className={s.content}><small className={s.small}>Hoag's Object</small> BOXX-3W Condos</p>
+          </li>
+          <li className={s.card} style={{backgroundImage: `url(${demiosImage})`}}>
+            <p className={s.content}><small className={s.small}>Demios, Mars</small> Kasai</p>
+          </li>
+          <li className={s.card} style={{backgroundImage: `url(${cassiniImage})`}}>
+            <p className={s.content}><small className={s.small}>Cassini, Jupiter</small> The Dustbowl</p>
+          </li>
+          <li className={s.card} style={{backgroundImage: `url(${ringImage})`}}>
+            <p className={s.content}><small className={s.small}>Outer Ring</small> Ptavvs</p>
+          </li>
+          <li className={s.card} style={{backgroundImage: `url(${enceladusImage})`}}>
+            <p className={s.content}><small className={s.small}>Enceladus</small> Ganymede</p>
+          </li>
+        </ul>
+      </Container>
     </div>;
   }
 }
